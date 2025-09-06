6357f96a99ff682656831c3de6bf4e8dd9cdd5a0
Vadym <djdim2008@gmail.com>
2025-09-06 18:43:33 +0200

    Initial Commit

---
+++ Timer/TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerApp.swift
+//  Timer
+//
+//  Created by vadym on 06.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TimerTests/TimerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TimerTests.swift
+//  TimerTests
+//
+//  Created by vadym on 06.09.2025.
+//
+
+import Testing
+@testable import Timer
+
+struct TimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
